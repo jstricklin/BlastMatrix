6b2ba3431791b81359c42c48db50163a00d9698b
jstricklin <stricklin.jose@gmail.com>
2019-11-08 01:46:28 -0700

    update camera and basic server connection

---
+++ Assets/Code/Networking/NetworkClient.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnitySocketIO;
+using UnitySocketIO.SocketIO;
+using UnitySocketIO.Events;
+using System;
+
+namespace Project.Networking 
+{
+    public class NetworkClient : SocketIOController
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            SetupEvents();
+            socketIO.Connect();
+        }
+
+        private void SetupEvents()
+        {
+            socketIO.On("open", (e) => {
+                Debug.Log("Connection to server established");
+            });
+            socketIO.On("register", (e) => {
+                var id = new JSONObject(e.data)["id"].str;
+                Debug.Log($"Client Registered with Server - Client ID: {id}");
+            });
+            socketIO.On("disconnected", (e) => {
+                var id = new JSONObject(e.data)["id"].str;
+                Debug.Log($"Player has disconnected: {id}");
+            });
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
