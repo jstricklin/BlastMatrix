e58c8ca56c3ddf3ef755c3387b87b95e5472e9ff
jstricklin <stricklin.jose@gmail.com>
2019-11-26 01:30:37 -0700

    start bloom effect

--- Assets/Code/Gameplay/CollisionDestroy.cs
+++ Assets/Code/Gameplay/CollisionDestroy.cs
@@ -16,11 +16,11 @@ namespace Project.Gameplay {
             if (ni == null || ni.GetID() != activator?.GetActivator())
             {
                 // Debug.Log("projectile network identity" + networkIdentity.GetID() + " and " + GetComponent<NetworkTransform>().player.id);
-                // gameObject.SetActive(false);
                 networkIdentity.GetSocket().Emit("collisionDestroy", JsonUtility.ToJson(new IDData()
                 {
                     id = networkIdentity.GetID(),
                 }));
+                gameObject.SetActive(false);
             }
         }
     }
