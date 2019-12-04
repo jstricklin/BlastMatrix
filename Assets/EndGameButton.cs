ce7aaf668840777eef8e89a0e20c692f8eabe5fa
jstricklin <stricklin.jose@gmail.com>
2019-12-03 19:56:00 -0700

    update logic and basic main menu

---
+++ Assets/EndGameButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Project.Networking;
+using UnityEngine;
+
+public class EndGameButton : MonoBehaviour
+{
+    public void ExitToMainMenu()
+    {
+        FindObjectOfType<NetworkClient>().ExitToMainMenu();
+    }
+
+}
