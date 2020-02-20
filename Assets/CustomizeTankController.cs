1794760344d0b0924ce0464cc275a7936bf5bbd9
jstricklin <stricklin.jose@gmail.com>
2020-02-20 00:19:21 -0700

    start custom tank logic

---
+++ Assets/CustomizeTankController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Project.Controllers {
+    public class CustomizeTankController : MonoBehaviour
+    {
+        Color primaryColor;
+        Color secondaryColor;
+
+        [SerializeField]
+        ScriptableObject colorSettings;
+        [SerializeField]
+        GameObject bodyBase;
+        [SerializeField]
+        GameObject cannonBase;
+        [SerializeField]
+        GameObject barrelBase;
+
+        [SerializeField]
+        ScriptableObject tank;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+
+
+        
+    }
+}
